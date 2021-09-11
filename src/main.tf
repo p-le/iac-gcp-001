3e0d0f570eef03e93fc8b26f97e3d52610df2dbd
Phu Le <phu.le.dev@gmail.com>
2021-09-11 07:31:38 +0000

    FIX terraform

--- src/main.tf
+++ src/main.tf
@@ -1,10 +1,10 @@
 resource "google_compute_network" "vpc" {
-  name                    = "${var.enviroment}-vpc"
+  name                    = "${var.environment}-vpc"
   auto_create_subnetworks = "false"
 }
 
 resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.region}-${var.enviroment}-subnet"
+  name          = "${var.region}-${var.environment}-subnet"
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.2.0.0/16"
